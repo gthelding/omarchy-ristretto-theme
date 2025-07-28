ee9971d4c9cc416ef75fce8a1f76f38d74cf9ddd
Greg Helding <greg@helding.net>
2025-07-27 19:01:16 -0500

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -4,6 +4,11 @@ return {
 		config = function()
 			require("monokai-pro").setup({
 				filter = "ristretto",
+				override = function()
+					return {
+						NonText = { fg = "#948a8b" },
+					}
+				end,
 			})
 			vim.cmd([[colorscheme monokai-pro]])
 		end,
