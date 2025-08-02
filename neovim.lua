c1135f39cc63b70e548d227fe7dd6377b103f98b
Greg Helding <158190040+gthelding@users.noreply.github.com>
2025-08-02 01:21:41 -0500

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,14 +1,20 @@
 return {
   {
     "gthelding/monokai-pro.nvim",
-    priority = 1000,
-    branch = "mini-icons",
     config = function()
       require("monokai-pro").setup({
         filter = "ristretto",
         override = function()
           return {
             NonText = { fg = "#948a8b" },
+            MiniIconsGrey = { fg = "#948a8b" },
+						MiniIconsRed = { fg = "#fd6883" },
+						MiniIconsBlue = { fg = "#85dacc" },
+						MiniIconsGreen = { fg = "#adda78" },
+						MiniIconsYellow = { fg = "#f9cc6c" },
+						MiniIconsOrange = { fg = "#f38d70" },
+						MiniIconsPurple = { fg = "#a8a9eb" },
+						MiniIconsCyan = { fg = "#85dacc" }, -- same value as MiniIconsBlue for consistency
           }
         end,
       })
