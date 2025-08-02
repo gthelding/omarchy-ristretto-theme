d8493b9831552c29c615b16a8355d0405bd37dfe
Greg Helding <158190040+gthelding@users.noreply.github.com>
2025-08-02 00:51:03 -0500

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,23 +1,24 @@
 return {
-	{
-		{ "gthelding/monokai-pro.nvim", priority = 1000 },
-		branch = "mini-icons",
-		config = function()
-			require("monokai-pro").setup({
-				filter = "ristretto",
-				override = function()
-					return {
-						NonText = { fg = "#948a8b" },
-					}
-				end,
-			})
-			vim.cmd([[colorscheme monokai-pro]])
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "monokai-pro",
-		},
-	},
+  {
+    "gthelding/monokai-pro.nvim",
+    priority = 1000,
+    branch = "mini-icons",
+    config = function()
+      require("monokai-pro").setup({
+        filter = "ristretto",
+        override = function()
+          return {
+            NonText = { fg = "#948a8b" },
+          }
+        end,
+      })
+      vim.cmd([[colorscheme monokai-pro]])
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
 }
